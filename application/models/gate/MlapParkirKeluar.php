709e77d92ca724bf53fff4b1cad4bd5ca39e788b
Ranur-react <rahmatnur845@gmail.com>
2021-11-30 11:09:57 +0700

    lapor day

--- application/models/gate/MlapParkirKeluar.php
+++ application/models/gate/MlapParkirKeluar.php
@@ -3,7 +3,13 @@ class MlapParkirKeluar extends CI_Model
 {
 	public function getallOneDay()
 	{
-		return $this->db->query("SELECT * FROM tb_exit WHERE date_exit=now()")->result_array();
+		$dateNow= date("d");
+		$montNow= date("m");
+		$yearNow= date("y");
+		// return $this->db->query("SELECT * FROM tb_exit WHERE DAY(date_exit)='$dateNow' AND MONTH(date_exit)='$montNow' AND YEAR(date_exit)='$yearNow';")->result_array();
+		return $this->db->query("SELECT * FROM tb_exit WHERE DAY(date_exit)='29' AND MONTH(date_exit)='11' AND YEAR(date_exit)='2021';")->result_array();
+
+		
 	}
 	public function getGroubOneDay()
 	{
